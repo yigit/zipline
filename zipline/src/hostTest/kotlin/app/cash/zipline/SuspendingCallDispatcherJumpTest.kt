a6890d2e04ef26fc62c0790e3a49c7474a601a69
yigit <yboyar@gmail.com>
2025-06-12 10:40:14 -0700

    failing test that reproduces the suspend function dispatcher jump issue

---
+++ zipline/src/hostTest/kotlin/app/cash/zipline/SuspendingCallDispatcherJumpTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2025 Cash App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.zipline
+
+import app.cash.zipline.testing.EchoRequest
+import app.cash.zipline.testing.SuspendingEchoService
+import app.cash.zipline.testing.loadTestingJs
+import app.cash.zipline.testing.singleThreadCoroutineDispatcher
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
+
+class SuspendingCallDispatcherJumpTest {
+  private val dispatcher = singleThreadCoroutineDispatcher(
+    name = "suspendingTest",
+  )
+  private val zipline = Zipline.create(dispatcher)
+
+  @BeforeTest
+  fun setUp() = runBlocking<Unit>(dispatcher) {
+    zipline.loadTestingJs()
+  }
+
+  @AfterTest
+  fun tearDown() = runBlocking<Unit>(dispatcher) {
+    zipline.close()
+  }
+
+  @OptIn(ExperimentalCoroutinesApi::class)
+  @Test
+  fun happyCode() = runTest {
+    val echoService = withContext(dispatcher) {
+      zipline.quickJs.evaluate("testing.app.cash.zipline.testing.prepareSuspendingJsBridges()")
+      zipline.quickJs.evaluate("testing.app.cash.zipline.testing.unblockSuspendingJs()")
+      zipline.take<SuspendingEchoService>("jsSuspendingEchoService")
+    }
+    val response = withContext(Dispatchers.IO) {
+      echoService.suspendingEcho(
+        EchoRequest("buddy")
+      )
+    }
+    assertEquals("hello from suspending JavaScript, buddy", response.message)
+  }
+}
